623febbfa3f2d34e71b5fe3f63066e9556e5bd5b
Tomas Sanchez <tomassanchezescobar@gmail.com>
2021-06-23 19:54:41 -0300

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDbqYTijxfEWEy52I1DeJ8kqnRiGGpFk60",
+    authDomain: "firegram-ce285.firebaseapp.com",
+    projectId: "firegram-ce285",
+    storageBucket: "firegram-ce285.appspot.com",
+    messagingSenderId: "1069046513730",
+    appId: "1:1069046513730:web:07f0eaa5fbe523b6fe2ddb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
